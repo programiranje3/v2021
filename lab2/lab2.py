eb29ee7115e9d32b38044a378809167ff0868a71
programiranje3 <p3python@gmail.com>
2021-10-29 14:24:48 +0200

    Solutions for Lab 2 added

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -7,7 +7,17 @@ TASK 1:
 Write a function that receives two lists and returns a new list that contains 
 only those elements (without duplicates) that appear in both input lists.
 """
-# def common_elements(l1, l2):
+def common_elements(l1, l2):
+    # Option 1
+    # new_list = list()
+    # for item in l1:
+    #     if (item in l2) and (item not in new_list):
+    #         new_list.append(item)
+    # return new_list
+    #
+    # Option 2
+    new_list = [item for item in l1 if item in l2]
+    return list(set(new_list))
 
 
 """
@@ -19,7 +29,15 @@ list1 = ["M", "na", "i", "Ke"]
 list2 = ["y", "me", "s", "lly"]
 Output: ['My', 'name', 'is', 'Kelly']
 """
-# def concat_index_wise(l1, l2):
+def concat_index_wise(l1, l2):
+    # Option 1
+    # new_list = list()
+    # for i, l1_item in enumerate(l1):
+    #     new_list.append(l1_item + l2[i])
+    # return new_list
+    #
+    # Option 2
+    return [i1+i2 for i1, i2 in zip(l1, l2)]
 
 
 """ 
@@ -30,7 +48,16 @@ Pangrams are sentences or phrases containing every letter of the alphabet at lea
 
 Hint: ascii_lowercase from the string module can be used to get all letters
 """
-# def pangram(string):
+def pangram(string):
+    from string import ascii_lowercase
+    # Option 1
+    # for l in ascii_lowercase:
+    #     if l not in string:
+    #         return False
+    # return True
+    #
+    # Option 2
+    return all([l in string for l in ascii_lowercase])
 
 
 """
@@ -41,7 +68,12 @@ followed by all upper case letters. Non-letter characters (if any) are ignored, 
 not included in the new 're-arranged' string.
 The new,'re-arranged' string is the function's return value.
 """
-# def rearrange_string(string):
+def rearrange_string(string):
+    rearranged = [ch for ch in string if ch.islower()]
+    upper = [ch for ch in string if ch.isupper()]
+    rearranged.extend(upper)
+    return "".join(rearranged)
+
 
 
 """
@@ -55,7 +87,19 @@ checks their validity using the following criteria:
 5. Length in the 6-12 range (including 6 and 12)
 Passwords that match the criteria should be printed in one line separated by a comma.
 """
-# def password_check(passwords):
+def password_check(passwords):
+    pass_to_check = [p.lstrip() for p in passwords.split(",")]
+    valid_passwords = []
+    for p in pass_to_check:
+        valid = [False] * 5
+        if any([ch.islower() for ch in p]): valid[0] = True
+        if any([ch.isdigit() for ch in p]): valid[1] = True
+        if any([ch.isupper() for ch in p]): valid[2] = True
+        if any([ch in '$#@' for ch in p]): valid[3] = True
+        if 5 < len(p) < 13: valid[4] = True
+        if all(valid): valid_passwords.append(p)
+    print(", ".join(valid_passwords))
+
 
 
 """
@@ -69,7 +113,17 @@ The function should process the report and print:
 - the proportion of servers that are down
 - names of servers that are down (if any)
 """
-# def server_status(report):
+def server_status(report):
+    down_servers = list()
+    lines = [line.lstrip() for line in report.split("\n") if line.strip()]
+    for line in lines:
+        _, s_name, _, status  = line.split()
+        if status.lower() == "down":
+            down_servers.append(s_name)
+    print(f"Number of servers menteioned in the report: {len(lines)}")
+    print(f"Proportion of down servers: {len(down_servers)/len(lines):.2f}")
+    print(f"Servers that are down: {','.join(down_servers)}")
+
 
 
 """
@@ -78,46 +132,63 @@ Write a function that finds numbers between 100 and 400 (both included)
 where each digit of a number is even. The numbers that match this criterion
 should be printed as a comma-separated sequence.
 """
+# Option 1
 # def numbers_with_all_even_digits():
+#     all_even = list()
+#     for num in range(100, 401):
+#         if all([ch in '02468' for ch in str(num)]):
+#             all_even.append(num)
+#     print(",".join([str(num) for num in all_even]))
 
+# Option 2
+def all_even_digits(num):
+    while(num > 0):
+        if (num % 10) % 2 != 0: return False
+        num = num // 10
+    return True
+
+def numbers_with_all_even_digits():
+    all_even = list()
+    for num in range(100, 401):
+        if all_even_digits(num): all_even.append(num)
+    print(",".join([str(num) for num in all_even]))
 
 
 
 if __name__ == '__main__':
-    pass
 
     # Task 1:
-    # a = [1, 1, 2, 3, 5, 8, 13, 21, 55, 89, 5, 10]
-    # b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    # print(common_elements(a,b))
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 55, 89, 5, 10]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    print(common_elements(a,b))
 
     # Task 2:
-    # list1 = ["M", "na", "i", "Ke"]
-    # list2 = ["y", "me", "s", "lly"]
-    # print(concat_index_wise(list1, list2))
+    list1 = ["M", "na", "i", "Ke"]
+    list2 = ["y", "me", "s", "lly"]
+    print(concat_index_wise(list1, list2))
 
     # Task 3:
-    # print("The quick brown fox jumps over the lazy dog")
-    # print(pangram("The quick brown fox jumps over the lazy dog"))
-    # print("The quick brown fox jumps over the lazy cat")
-    # print(pangram("The quick brown fox jumps over the lazy cat"))
+    print("The quick brown fox jumps over the lazy dog")
+    print(pangram("The quick brown fox jumps over the lazy dog"))
+    print("The quick brown fox jumps over the lazy cat")
+    print(pangram("The quick brown fox jumps over the lazy cat"))
 
     # Task 4:
-    # print(("Rearranging string: 'PyNaTive_2021'"))
-    # print(rearrange_string("PyNaTive_2021"))
+    print(("Rearranging string: 'PyNaTive_2021'"))
+    print(rearrange_string("PyNaTive_2021"))
 
     # Task 5:
-    # print("Passwords to check: ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR")
-    # password_check("ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR")
+    print("Passwords to check: ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR")
+    password_check("ABd1234@1, a F1#, 2w3E*, 2We334#5, t_456WR")
 
     # Task 6:
-    # sample_input = '''
-    #     Server abc01 is up
-    #     Server abc02 is down
-    #     Server xyz01 is down
-    #     Server xyz02 is up
-    #     '''
-    # server_status(sample_input)
+    sample_input = '''
+        Server abc01 is up
+        Server abc02 is down
+        Server xyz01 is down
+        Server xyz02 is up
+        '''
+    server_status(sample_input)
 
     # Task 7:
-    # numbers_with_all_even_digits()
+    numbers_with_all_even_digits()
