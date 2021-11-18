c5d978155e4facb7e59acdc5e8dcfb741560841c
programiranje3 <p3python@gmail.com>
2021-11-18 22:52:22 +0100

    Added solutions for Lab 5

--- lab5/flight.py
+++ lab5/flight.py
@@ -30,55 +30,130 @@ Create the Flight class with the following elements:
 
 """
 
+from datetime import datetime
+from lab5.passenger import Passenger
+
+class Flight:
+
+    departure_format = "%Y-%m-%d %H:%M"
+
+    def __init__(self, flight_num, departure):
+        self.flight_num = flight_num
+        self.departure = departure
+        self.passengers = list()
+
+
+    @property
+    def departure(self):
+        return self.__departure
+
+    @departure.setter
+    def departure(self, value):
+        if isinstance(value, datetime):
+            if value > datetime.now():
+                self.__departure = value
+                return
+        if isinstance(value, str):
+            value = datetime.strptime(value, self.departure_format)
+            if value and value > datetime.now():
+                self.__departure = value
+                return
+        print(f"Departure must refer to a future date and time")
+        self.__departure = None
+
+
+    def add_passenger(self, p):
+        if type(p) is not Passenger:
+            print(f"Error! Wrong input: expected a Passenger object, got {type(p)} object")
+            return
+        if (p not in self.passengers) and p.is_COVID_safe:
+            self.passengers.append(p)
+            print(f"Successfully added: {p}")
+        else:
+            print(f"Error, passenger {p.name} could not be added, since the passenger "
+                  f"{'is already in the passengers list' if p in self.passengers else 'does not have a valid COVID permit'}")
+
+    def __str__(self):
+        flight_str = f"Data about flight number {self.flight_num}:\n"
+        flight_str += f"Scheduled for departure at {datetime.strftime(self.departure, self.departure_format)}.\n" \
+            if self.departure else "Departure date and time still unknown.\n"
+        if len(self.passengers) == 0:
+            flight_str += "Still no registered passengers."
+        else:
+            flight_str += "Passengers:\n" + "\n".join([str(p) for p in self.passengers])
+        return flight_str
+
+
+    def time_till_departure(self):
+        if self.departure:
+            time_left = self.departure - datetime.now()
+            hours_left, rest_sec = divmod(time_left.seconds, 3600)
+            mins_left, rest_sec = divmod(rest_sec, 60)
+            return time_left.days, hours_left, mins_left
+
+        print("Departure time is still unknown")
+        return None
+
+
+    def __iter__(self):
+        self.__iter_counter = 0
+        return self
+
+    def __next__(self):
+        if self.__iter_counter == len(self.passengers):
+            raise StopIteration
+        next_passenger = self.passengers[self.__iter_counter]
+        self.__iter_counter += 1
+        return next_passenger
+
 
 if __name__ == '__main__':
 
-    pass
+    lh1411 = Flight('LF1411', '2021-12-05 6:50')
+    lh992 = Flight('LH992', '2021-11-25 12:20')
 
-    # lh1411 = Flight('LF1411', '2021-12-05 6:50')
-    # lh992 = Flight('LH992', '2021-11-25 12:20')
-    #
-    # print("\nFLIGHTS DATA:\n")
-    # print(lh1411)
-    # print()
-    # print(lh992)
-    #
-    # print()
-    #
-    # bob = Passenger("Bob Smith", "Serbia", "123456", True)
-    # john = Passenger("John Smith", "Spain", 987656, True)
-    # jane = Passenger("Jane Smith", "Italy", "987659")
-    # mike = Passenger.covid_free_Aussie_passenger("Mike Brown", "123654")
-    #
-    # for p in [bob, john, jane, mike]:
-    #     lh1411.add_passenger(p)
-    #
-    # lh1411.add_passenger(Passenger("Jane Smith", "USA", "987659", False))
-    # print()
-    #
-    # print(f"\nFLIGHTS DATA AFTER ADDING PASSENGERS TO THE FLIGHT {lh1411.flight_num}:\n")
-    # print(lh1411)
-    #
-    # print()
-    #
-    # days, hours, mins = lh1411.time_till_departure()
-    # print(f"Time till departure of the flight {lh1411.flight_num}: {days} days, {hours} hours, and {mins} minutes")
-    #
-    # print()
-    # print("\nPASSENGERS ON FLIGHT LH1411:")
-    # flight_iter = iter(lh1411)
-    #
-    # try:
-    #     while True:
-    #         print(next(flight_iter))
-    # except StopIteration:
-    #     print("No more passengers")
-    #
-    # print()
-    #
-    # print("\nPASSENGERS ON FLIGHT LH1411 (FOR loop):")
-    # for passenger in lh1411:
-    #     print(passenger)
+    print("\nFLIGHTS DATA:\n")
+    print(lh1411)
+    print()
+    print(lh992)
+
+    print()
+
+    bob = Passenger("Bob Smith", "Serbia", "123456", True)
+    john = Passenger("John Smith", "Spain", 987656, True)
+    jane = Passenger("Jane Smith", "Italy", "987659")
+    mike = Passenger.covid_free_Aussie_passenger("Mike Brown", "123654")
+
+    for p in [bob, john, jane, mike]:
+        lh1411.add_passenger(p)
+
+    print("\nTRYING TO ADD A PASSENGER WHOIS ALREADY IN THE LIST")
+    lh1411.add_passenger(Passenger("J Smith", "Spain", "987656", True))
+    print()
+
+    print(f"\nFLIGHTS DATA AFTER ADDING PASSENGERS TO THE FLIGHT {lh1411.flight_num}:\n")
+    print(lh1411)
+
+    print()
+
+    days, hours, mins = lh1411.time_till_departure()
+    print(f"Time till departure of the flight {lh1411.flight_num}: {days} days, {hours} hours, and {mins} minutes")
+
+    print()
+    print("\nPASSENGERS ON FLIGHT LH1411:")
+    flight_iter = iter(lh1411)
+
+    try:
+        while True:
+            print(next(flight_iter))
+    except StopIteration:
+        print("No more passengers")
+
+    print()
+
+    print("\nPASSENGERS ON FLIGHT LH1411 (FOR loop):")
+    for passenger in lh1411:
+        print(passenger)
 
 
 
--- lab5/passenger.py
+++ lab5/passenger.py
@@ -37,30 +37,104 @@ Create the Passenger class with the following methods:
   are considered the same if they are citizens of the same country and have the same passport number
 """
 
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+
+class Passenger:
+
+    def __init__(self, name, country, passport, is_covid_safe=False):
+        self.name = name
+        self.country = country
+        self.passport = passport
+        self.is_COVID_safe = is_covid_safe
+
+    @property
+    def passport(self):
+        return self.__passport
+
+    @passport.setter
+    def passport(self, value):
+        if isinstance(value, str) and len(value) == 6 and all([ch.isdigit() for ch in value]):
+            self.__passport = value
+            return
+        if isinstance(value, int) and len(str(value)) == 6:
+            self.__passport = str(value)
+            return
+        print(f"Invalid passport number ({value})")
+        self.__passport = None
+
+
+    def __str__(self):
+        passenger_str = f"Passenger {self.name} from {self.country} and passport number " \
+                        f"{self.__passport if self.__passport else 'unknown'}"
+        passenger_str += f"; {'has' if self.is_COVID_safe else 'does NOT have'} valid COVID permit"
+        return passenger_str
+
+
+    def update_covid_safe(self, evidence_type, evidence_date):
+        self.is_COVID_safe = False
+        if evidence_type.lower() not in ['vaccinated', 'tested_negative']:
+            print(f"Invalid evidence type ({evidence_type}). Cannot proceed!")
+            return
+        evidence_date = self.get_date(evidence_date)
+        if not evidence_date:
+            print(f"Invalid evidence date ({evidence_date}). Cannot proceed!")
+            return
+        if evidence_type.lower() == 'tested_negative':
+            self.is_COVID_safe = evidence_date + timedelta(days=3) > datetime.now()
+        else:
+            self.is_COVID_safe = evidence_date + relativedelta(months=6) > datetime.now()
+
+
+    @staticmethod
+    def get_date(date_val):
+        if isinstance(date_val, (date, datetime)):
+            return date_val
+        elif isinstance(date_val, str):
+            return datetime.strptime(date_val, "%d/%m/%Y")
+        return None
+
+
+    @classmethod
+    def covid_free_Aussie_passenger(cls, name, passport):
+        return cls(name, 'Australia', passport, True)
+
+
+    def __eq__(self, other):
+        # Option 1
+        # if not isinstance(other, Passenger):
+        #     return False
+        # Option 2
+        if type(self) is not type(other):
+            return False
+        if self.passport and other.passport and self.country and other.country:
+            return other.passport == self.passport and other.country == self.country
+        else:
+            print(f"Required data is missing for at least one passenger -> Cannot verify identity")
+            return False
+
 
 if __name__ == '__main__':
 
-    pass
+    bob = Passenger("Bob Smith", "Serbia", "123456", True)
+    john = Passenger("John Smith", "Spain", 987656, True)
+    jane = Passenger("Jane Smith", "Italy", "987659")
+    mike = Passenger.covid_free_Aussie_passenger("Mike Brown", "123654")
 
-    # bob = Passenger("Bob Smith", "Serbia", "123456", True)
-    # john = Passenger("John Smith", "Spain", 987656, True)
-    # jane = Passenger("Jane Smith", "Italy", "987659")
-    # mike = Passenger.covid_free_Aussie_passenger("Mike Brown", "123654")
-    #
-    # print("PASSENGERS DATA:\n")
-    # print(bob)
-    # print(john)
-    # print(jane)
-    # print(mike)
-    #
-    # print()
-    # print("Checking if 'bob' and 'john' refer to the same passenger")
-    # print(bob == john)
-    #
-    # print("\nPASSENGERS DATA AFTER CHECKING COVID PERMITS:\n")
-    # jane.update_covid_safe('vaccinated', '12/11/2021')
-    # print(jane)
-    #
-    # bob.update_covid_safe('tested_negative', '10/11/2021')
-    # print(bob)
+    print("PASSENGERS DATA:\n")
+    print(bob)
+    print(john)
+    print(jane)
+    print(mike)
+
+    print()
+    print("Checking if 'bob' and 'john' refer to the same passenger")
+    print(bob == john)
+
+    print("\nPASSENGERS DATA AFTER CHECKING COVID PERMITS:\n")
+    jane.update_covid_safe('vaccinated', '12/11/2021')
+    print(jane)
+
+    bob.update_covid_safe('tested_negative', '10/11/2021')
+    print(bob)
 
